36b33dd6bcecc9aaa22ce9fe3f6fa1691cc78d07
Max Persson <max.e.persson@live.se>
2022-02-09 11:16:39 +0100

    Changed the code in the CI-server and removed C0103 from the linting rules. This relates to issues #1 and #15.

--- ci_server/server.py
+++ ci_server/server.py
@@ -6,7 +6,7 @@ class CIServer(BaseHTTPRequestHandler):
     TODO: implement the CI server, this is just a placeholder.
     """
 
-    def do_get(self):
+    def do_GET(self):
         if self.path == "/":
             self.path = "../static/ci_server/index.html"
         try:
@@ -18,10 +18,9 @@ class CIServer(BaseHTTPRequestHandler):
 
         self.end_headers()
         self.wfile.write(bytes(file, "utf8"))
-        file.close() # Remember to close files.
         # TODO: implement logic for serving files here
 
-    def do_post(self):
+    def do_POST(self):
         # TODO: implement CI logic here
         self.send_response(404)
         self.end_headers()
