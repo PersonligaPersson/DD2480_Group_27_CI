8bd6ee7a0c7a486ceb2af1642f7e1a882c8f96f6
mellonnen <arvidgotthard98@gmail.com>
2022-02-10 14:48:49 +0100

    fix: pylint fixes and general cleanup

--- ci_server/server.py
+++ ci_server/server.py
@@ -17,7 +17,15 @@ PATH_TO_CLONED_BRANCHES = "branches"
 
 
 class CIServer:
-    # Constructor
+    """
+    CIServer
+
+    Attributes:
+        handler: A function that returns a CIServerHandler.
+        address: The address that the server runs on.
+        port:  The port that the server runs on.
+    """
+
     def __init__(self, address, port):
         # closure that will instantiate a instance a CIServerHandler for us.
         def handler_fn(*args):
@@ -37,7 +45,7 @@ class CIServer:
             print("\nclosing server...")
             httpd.server_close()
 
-    def update_commit_status(repo, sha, status):
+    def update_commit_status(self, repo, sha, status):
         load_dotenv()
         TOKEN = os.getenv("GITHUB_TOKEN")
         HEADERS = {"Authorization": "token " + TOKEN}
@@ -53,14 +61,17 @@ class CIServer:
 
 
 class CIServerHandler(BaseHTTPRequestHandler):
+
+    """
+    Custom HTTP handler that handles post request from webhook on Github.
+    """
+
     def __init__(self, update_commit_status_fn, *args):
         # We assign the function that updates the commit statuses to a class member.
         self.update_commit_status = update_commit_status_fn
         super().__init__(*args)
 
-    """
-    TODO: implement the CI server, this is just a placeholder.
-    """
+    # GET/POST handler
 
     def do_GET(self):
         if self.path == "/":
@@ -76,19 +87,37 @@ class CIServerHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes(file, "utf8"))
         # TODO: implement logic for serving files here
 
-    # The purpose of this function is to trigger a shell script that lints the
-    # code in the project.
-    # Note: This should be update so that it runs the linting process on the
-    # correct code. As it is it will lint all py files.
+    def do_POST(self):
+        content_length = int(
+            self.headers["Content-Length"]
+        )  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        data = json.loads(post_data)
 
-    # A shellscript must be explicitly set to executable using the chmod command.
-    # Example: chmod +x ./runLint.sh
-    def run_lint():
-        runLintPath = "../shellscripts/runLint.sh"
-        proc = subprocess.run([runLintPath, ""], shell=True, check=True)
-        return proc.stdout
-        # verify the signature of the message
+        # verify signature
+        if self.verify_signature(post_data):
+            print("--------------------------------------------------")
+            print("WRONG SIGNATURE")
+            self.send_custom_response(401, "Wrong signature")
+            return
+        else:
+            print("--------------------------------------------------")
+            print("SIGNATURE VERIFIED")
+            print("--------------------------------------------------")
 
+        # handle push event
+        if self.headers["X-GitHub-Event"] == "push":
+            if self.push_handler(data):
+                return
+
+        # send success message
+        self.send_custom_response(200, "SUCCESS")
+
+    # HELPER LOGIC
+
+    # REQUEST HELPERS
+
+    # Verify that it is the webhook that is talking to us.
     def verify_signature(self, post_data):
         sha_name, signature = self.headers["X-Hub-Signature-256"].split("=")
         if sha_name != "sha256":
@@ -98,6 +127,68 @@ class CIServerHandler(BaseHTTPRequestHandler):
             os.getenv("secretToken").encode(), msg=post_data, digestmod=hashlib.sha256
         ).hexdigest()
         return not hmac.compare_digest(local_signature, signature)
+
+    # handle push event
+    def push_handler(self, data):
+        # clone specific branch
+        print("--------------------------------------------------")
+        print("CLONING BRANCH")
+        print("--------------------------------------------------")
+        if self.clone_branch(data):
+            print("--------------------------------------------------")
+            print("COULDN'T CLONE THE BRANCH")
+            self.send_custom_response(500, "Couldn't clone the branch")
+            return ERROR
+        else:
+            print("--------------------------------------------------")
+            print("BRANCH SUCCESSFULLY CLONED")
+            print("--------------------------------------------------")
+            return NO_ERROR
+
+    # send a custom response given a HTTP code and a specific message
+    def send_custom_response(self, code, msg):
+        print("--------------------------------------------------")
+        print("SENDING RESPONSE")
+        self.send_response(code)
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Length", str(len(msg)))
+        self.wfile.write(msg.encode("utf-8"))
+        self.end_headers()
+        print("--------------------------------------------------")
+        print("\n")
+
+    # CLONING
+
+    # clone the branch related to the push event
+    def clone_branch(self, data):
+        # retrieve clone url and branch name
+        clone_url = data["repository"]["clone_url"]
+        branch = data["ref"].split("/")[-1]
+        commit_id = data["commits"][0]["id"]
+        return os.system(
+            f"git clone --single-branch --depth 1 -b {branch} {clone_url} {PATH_TO_CLONED_BRANCHES}/{commit_id}"
+        )
+
+    # remove the cloned branch of the given commit id
+    def remove_cloned_branch(self, commit_id):
+        return os.system(f"rm -rf {PATH_TO_CLONED_BRANCHES}/{commit_id}")
+
+    # LINTING
+
+    # The purpose of this function is to trigger a shell script that lints the
+    # code in the project.
+    # Note: This should be update so that it runs the linting process on the
+    # correct code. As it is it will lint all py files.
+
+    # A shellscript must be explicitly set to executable using the chmod command.
+    # Example: chmod +x ./runLint.sh
+    def run_lint(self):
+        runLintPath = "../shellscripts/runLint.sh"
+        proc = subprocess.run([runLintPath, ""], shell=True, check=True)
+        return proc.stdout
+        # verify the signature of the message
+
+    # LOGGING
 
     # The purpose of this function is to generate a unique identifier that serves
     # as the build log name.
@@ -128,72 +219,3 @@ class CIServerHandler(BaseHTTPRequestHandler):
             f.write(
                 f"=== LINT OUTPUT ===\n{lint_output}\n\n=== PYTEST OUTPUT ===\n{pytest_output}\n"
             )
-
-    # clone the branch related to the push event
-    def clone_branch(self, data):
-        # retrieve clone url and branch name
-        clone_url = data["repository"]["clone_url"]
-        branch = data["ref"].split("/")[-1]
-        commit_id = data["commits"][0]["id"]
-        return os.system(
-            f"git clone --single-branch --depth 1 -b {branch} {clone_url} {PATH_TO_CLONED_BRANCHES}/{commit_id}"
-        )
-
-    # send a custom response given a html code and a specific message
-    def send_custom_response(self, code, msg):
-        print("--------------------------------------------------")
-        print("SENDING RESPONSE")
-        self.send_response(code)
-        self.send_header("Content-Type", "text/plain")
-        self.send_header("Content-Length", str(len(msg)))
-        self.wfile.write(msg.encode("utf-8"))
-        self.end_headers()
-        print("--------------------------------------------------")
-        print("\n")
-
-    # handle push event
-    def push_handler(self, data):
-        # clone specific branch
-        print("--------------------------------------------------")
-        print("CLONING BRANCH")
-        print("--------------------------------------------------")
-        if self.clone_branch(data):
-            print("--------------------------------------------------")
-            print("COULDN'T CLONE THE BRANCH")
-            self.send_custom_response(500, "Couldn't clone the branch")
-            return ERROR
-        else:
-            print("--------------------------------------------------")
-            print("BRANCH SUCCESSFULLY CLONED")
-            print("--------------------------------------------------")
-            return NO_ERROR
-
-    # remove the cloned branch of the given commit id
-    def remove_cloned_branch(self, commit_id):
-        return os.system(f"rm -rf {PATH_TO_CLONED_BRANCHES}/{commit_id}")
-
-    def do_POST(self):
-        content_length = int(
-            self.headers["Content-Length"]
-        )  # <--- Gets the size of data
-        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
-        data = json.loads(post_data)
-
-        # verify signature
-        if self.verify_signature(post_data):
-            print("--------------------------------------------------")
-            print("WRONG SIGNATURE")
-            self.send_custom_response(401, "Wrong signature")
-            return
-        else:
-            print("--------------------------------------------------")
-            print("SIGNATURE VERIFIED")
-            print("--------------------------------------------------")
-
-        # handle push event
-        if self.headers["X-GitHub-Event"] == "push":
-            if self.push_handler(data):
-                return
-
-        # send success message
-        self.send_custom_response(200, "SUCCESS")
